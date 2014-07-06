69b1e25483e79e32107c01d0134b3a3f9e3139b4
Amanda Steinwedel <amanda.steinwedel@gmail.com>
2014-07-06 16:47:09 -0500

    Initial django commit

---
+++ hogwarts/library/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+# Create your models here.
+class Characters(models.Model):
+
+class Spells(models.Model):
+
+class Creatures(models.Model):
+
+class Potions(models.Model):
+
+class Locations(models.Model):
+
+class Schools(Locations):
+
+class Shops(Locations):
+
+class Stores(models.Model):
+
+class Artifacts(models.Model):
+
+class Books(models.Model):
+
+class AcademicStatuses(models.Model):
+
+class Relationships(models.Model):
