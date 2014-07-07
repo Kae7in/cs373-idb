953676caf02a52b73e90b3c94060e338f9ff0893
Isabella Bhardwaj <isabella@cs.utexas.edu>
2014-07-06 19:43:32 -0500

    Started Potion tests

--- hogwarts/library/tests.py
+++ hogwarts/library/tests.py
@@ -1,5 +1,6 @@
 from django.test import TestCase
 from library.models import Creature
+from library.models import Potion
 
 # Create your tests here.
 class CreatureTest(TestCase):
@@ -26,3 +27,36 @@ class CreatureTest(TestCase):
         self.assertEquals(only_creature.description, only_creature.description)
         self.assertEquals(only_creature.classification, creature.classification)
         self.assertEquals(only_creature.rating, creature.rating)
+
+class PotionTest(TestCase):
+    def test_create_potion(self):
+        potion = Potion()
+        name = "Amortentia"
+        potion.title = name
+        difficult = 'A'
+        potion.difficulty = difficult
+        physical_description = 'Its color is molten gold. When inside a cauldron, drops of the liquid will leap above the surface.'
+        potion.description = physical_description
+        the_recipe = 'Add to the cauldron an Ashwinder egg and horseradish before heating. Then add a juiced squill bulb and stir. Add chopped Murtlap tentacle to mixture and heat again. Then add a dash of tincture of thyme before stirring slowly. Add ground Occamy eggshell. Stir slowly and heat again. Sprinkle powdered common rue over the  mixture before stirring and heating again. Then make a figure-eight motion over the cauldron with the wand and say the incantation, "Felixempra!". Needs to be brewed for six months.'
+        potion.recipe = the_recipe
+        effect = "Increases the drinker's luck. Overdose can induce giddiness and recklessness. Large quantities can be toxic."
+        potion.effects = effect
+        uses = "Hermoine, Ginny and Ron used Felix Felices to evade the curses of Death Eaters.  Horace Slughorn used this potion at 24and 57 years old. It gave him one perfect day each time. Professor Slughorn awarded Harry enough potion for 12 hours' worth of luck for producing the most effective Draught of Living Death potion of anyone in his class."
+        potion.usages = uses
+        more_information = "Also called \"Liquid Luck\", Felix Felices was invented by Zygmunt Budge in the 16th century. The drinker of the potion is given the best scenario possible in their situation, which manifests in an urge or a voice telling them which action to take to ensure the outcome."
+        
+        potion.more_info = more_information
+        potion.save()
+        potions = Potion.objects.all()
+        self.assertEquals(len(potions), 1)
+        potion_created = potions[0]
+        self.assertEquals(potion, potion_created)
+        
+        
+        self.assertEquals(name, potion.title)
+        self.assertEquals(difficult, potion.difficulty)
+        self.assertEquals(physical_description, potion.description)
+        self.assertEquals(the_recipe, potion.recipe)
+        self.assertEquals(effect, potion.effects)
+        self.assertEquals(uses, potion.usages)
+        self.assertEquals(more_information, potion.more_info)
