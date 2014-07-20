015f0c056b82819bd161f90855716afa0ec40e29
Angela Hsu <ayh123@gmail.com>
2014-07-19 19:13:50 -0500

    used regex to map topic detailed pages

--- hogwarts/marauder/views.py
+++ hogwarts/marauder/views.py
@@ -1,1 +1,7 @@
 from django.shortcuts import render
+
+def index(request):
+    return render(request, 'index.html')
+
+def creature_detail(request, topic, creature_id):
+    return render(request, '{0}/{1}.html'.format(topic, creature_id))
