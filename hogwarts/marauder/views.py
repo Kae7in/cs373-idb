3fc728aba276ae9bb7a2ba10dffbe84e0b501b21
Amanda Steinwedel <amanda.steinwedel@gmail.com>
2014-07-22 18:50:54 -0500

    adding dynamic character template and routes

--- hogwarts/marauder/views.py
+++ hogwarts/marauder/views.py
@@ -5,26 +5,59 @@ from marauder.models import *
 class IndexView(generic.ListView):
     template_name = 'index.html'
 
-class CreatureView(generic.DetailView):
+# Creature Views
+class CreatureListView(generic.ListView):
+    model = Creature
+    template_name = 'creatures/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CreatureListView, self).get_context_data(**kwargs)
+        return context
+
+class CreatureDetailView(generic.DetailView):
     model = Creature
     template_name = 'creatures/base.html'
 
-class CharacterView(generic.DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(CreatureDetailView, self).get_context_data(**kwargs)
+        return context
+
+# Character Views
+class CharacterListView(generic.ListView):
+    model = Character
+    template_name = 'characters/index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(CharacterListView, self).get_context_data(**kwargs)
+        return context
+
+class CharacterDetailView(generic.DetailView):
     model = Character
     template_name = 'characters/base.html'
 
-class SpellsView(generic.DetailView):
+    def get_context_data(self, **kwargs):
+        context = super(CharacterDetailView, self).get_context_data(**kwargs)
+        return context
+
+# Spell Views
+class SpellDetailView(generic.DetailView):
     model = Spell
     template_name = 'spells/base.html'
 
-class PotionsView(generic.DetailView):
+# Potion Views
+class PotionDetailView(generic.DetailView):
     model = Potion
     template_name = 'potions/base.html'
 
-class StoriesView(generic.DetailView):
+# Story Views
+class StoryDetailView(generic.DetailView):
     model = Story
     template_name = 'stories/base.html'
 
-class ArtifactsView(generic.DetailView):
+# Artifact Views
+class ArtifactDetailView(generic.DetailView):
     model = Artifact
     template_name = 'artifacts/base.html'
+
+# Book Views
+# Location Views
