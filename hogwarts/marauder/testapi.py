1716c055d633beaa6d52d100f0ae626eeb6c957e
Isabella <isabella11@utexas.edu>
2014-07-23 14:15:50 -0500

    Added potion api unit tests

--- hogwarts/marauder/testapi.py
+++ hogwarts/marauder/testapi.py
@@ -51,8 +51,60 @@ class TestCharacterAPI(TestCase):
         self.assertEqual(response, expected)
 
     def testIndex(self):
-        all = Character.objects.all()
+        all_characters = Character.objects.all()
         url = reverse('characters_api')
         response = self.fetch_url(url)
         expected = [{'quote_by': 'Albus Dumbledore', 'id': 1, 'wand': '15 inch elder wood with thestral hair', 'quote': "Why doesn't anyone ever give me warm socks?", 'description': 'Albus Dumbledore was the only wizard Voldemort was wary of.', 'name': 'Albus Dumbledore', 'magical': True}, {'quote_by': 'neville on the battlefield', 'id': 24, 'wand': 'a very good wand', 'magical': True, 'quote': 'Die, basilisk!', 'description': 'Neville became a surprisingly attractive teenager.', 'name': 'Neville Longbottom'}]
+        self.assertEqual(response, expected)
+
+
+class TestPotionAPI(TestCase):
+
+    @classmethod
+    def setUpClass(self):
+        p = Potion.objects.create(
+            id=1,
+            name='AmortentiaTest',
+            difficulty='Advanced',
+            physical_description='Steam rises from Amortentia in coil-like designs.',
+            effects='Amortentia is the most powerful of the love potions.',
+            recipe='The ingredients are 7 rose thorns...',
+            notable_uses='Department of Mysteries'
+        )
+        p2 = Potion.objects.create(
+            id=24,
+            name='VeritaserumTest',
+            difficulty='Advanced',
+            physical_description='No color and no odor.',
+            effects='You have to tell the truth.',
+            recipe='Unknown',
+            notable_uses='Umbridge'
+        )
+
+    def fetch_url(self, url):
+        client = Client()
+        response = client.get(url)
+        return json.loads(response.content.decode("utf-8"))
+
+
+    def testDetail(self):
+        p = Potion.objects.all()[0]
+        url = reverse('potions_api', kwargs={'id': p.id})
+        response = self.fetch_url(url)
+        expected = {
+            "id": p.id, 
+            "name": p.name, 
+            "difficulty": p.wand if p.wand else None,
+            "physical_description": p.description,
+            "effects": p.magical,
+            "recipe": p.quote if p.quote else None,
+            "notable_uses": p.quote_by if p.quote_by else None
+        }
+        self.assertEqual(response, expected)
+
+    def testIndex(self):
+        all_potions = Potions.objects.all()
+        url = reverse('potions_api')
+        response = self.fetch_url(url)
+        expected = [{'id':1, 'name': 'AmortentiaTest', 'difficulty':'Advanced', 'physical_description':'Steam rises from Amortentia in coil-like designs', 'effects':'Amortentia is the most powerful of the love potions', 'recipe':'The ingredients are 7 rose thorns...', 'notable_uses':'Department of Mysteries'}, {'id':24, 'name':'VeritaserumTest', 'difficulty':'Advanced', 'physical_description':'No color and no odor.', 'effects':'You have to tell the truth.', 'recipe':'Unknown', 'notable_uses':'Umbridge'}]
         self.assertEqual(response, expected)
