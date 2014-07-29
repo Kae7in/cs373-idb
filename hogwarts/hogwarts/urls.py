afc73972d190dbaf84120154f25e0d15a486b19a
David Moench <domoench@gmail.com>
2014-07-29 18:37:07 -0500

    Setup search with Haystack - forgot these files on the last commit.

--- hogwarts/hogwarts/urls.py
+++ hogwarts/hogwarts/urls.py
@@ -10,4 +10,7 @@ urlpatterns = patterns('',
 
     url(r'^admin/', include(admin.site.urls)),
     url(r'^', include('marauder.urls')),
+
+    # Haystack
+    (r'^search/', include('haystack.urls')),
 )
