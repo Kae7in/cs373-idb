dc4aee0e3ac95f70c3d8700e920fd05622512de4
Angela Hsu <ahsu@cs.utexas.edu>
2014-08-01 18:17:58 -0500

    added locations; some css formatting

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -34,3 +34,23 @@ class CreatureIndex(indexes.SearchIndex, indexes.Indexable):
       that don't need refiltering?
       """
       return self.get_model().objects
+
+class LocationIndex(indexes.SearchIndex, indexes.Indexable):
+  # Declare which model attributes are included in indexing/searching
+  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
+  name = indexes.CharField(model_attr='name')
+  kind = indexes.CharField(model_attr='kind')
+  description = indexes.CharField(model_attr='description')
+
+  def get_model(self):
+      return Location
+
+  def index_queryset(self, using=None):
+      """
+      Used when the entire index for model is updated. You can filter out things
+      that don't need refiltering?
+      """
+      return self.get_model().objects
+
+
+ 
