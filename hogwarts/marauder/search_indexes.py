b8208c2f5a9c03e5ed62751a95ee5f084b46ff31
Isabella <isabella11@utexas.edu>
2014-08-01 17:58:48 -0500

    Added search models for Ingredient and Book

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -52,3 +52,44 @@ class PotionIndex(indexes.SearchIndex, indexes.Indexable):
       that don't need refiltering?
       """
       return self.get_model().objects
+
+
+class IngredientIndex(indexes.SearchIndex, indexes.Indexable):
+  # Declare which model attributes are included in indexing/searching
+  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
+  name = indexes.CharField(model_attr='name')
+  creature = indexes.CharField(model_attr='creature')
+
+  def get_model(self):
+      return Ingredient
+
+  def prepare_author(self, obj):
+    return obj.creature.name
+
+  def index_queryset(self, using=None):
+      """
+      Used when the entire index for model is updated. You can filter out things
+      that don't need refiltering?
+      """
+      return self.get_model().objects
+
+
+class BookIndex(indexes.SearchIndex, indexes.Indexable):
+  # Declare which model attributes are included in indexing/searching
+  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
+  name = indexes.CharField(model_attr='name')
+  author = indexes.CharField(model_attr='author')
+  subjects = indexes.MultiValueField()
+
+  def get_model(self):
+      return Book
+
+  def prepare_subjects(self, obj):
+    return [c.name for c in obj.subjects.all()]
+
+  def index_queryset(self, using=None):
+      """
+      Used when the entire index for model is updated. You can filter out things
+      that don't need refiltering?
+      """
+      return self.get_model().objects
