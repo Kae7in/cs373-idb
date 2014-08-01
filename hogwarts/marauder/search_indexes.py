998bdaf742a9886493a77a065a2514cd1771bb2b
Isabella <isabella11@utexas.edu>
2014-08-01 17:10:08 -0500

    Added PotionIndexes

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -34,3 +34,21 @@ class CreatureIndex(indexes.SearchIndex, indexes.Indexable):
       that don't need refiltering?
       """
       return self.get_model().objects
+
+
+class PotionIndex(indexes.SearchIndex, indexes.Indexable):
+  # Declare which model attributes are included in indexing/searching
+  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
+  title = indexes.CharField(model_attr='title')
+  effects = indexes.CharField(model_attr='effects')
+  recipe = indexes.CharField(model_attr='recipe')
+
+  def get_model(self):
+      return Potion
+
+  def index_queryset(self, using=None):
+      """
+      Used when the entire index for model is updated. You can filter out things
+      that don't need refiltering?
+      """
+      return self.get_model().objects
