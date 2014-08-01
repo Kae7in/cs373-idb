c58b7ff65c13a50f77017599190a92407d4f2a6e
Angela Hsu <ahsu@cs.utexas.edu>
2014-08-01 13:16:25 -0500

    trying to get search highlighting to work

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -6,9 +6,27 @@ class CharacterIndex(indexes.SearchIndex, indexes.Indexable):
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
   name = indexes.CharField(model_attr='name')
   wand = indexes.CharField(model_attr='wand')
+  description = indexes.CharField(model_attr='description')
 
   def get_model(self):
       return Character
+
+  def index_queryset(self, using=None):
+      """
+      Used when the entire index for model is updated. You can filter out things
+      that don't need refiltering?
+      """
+      return self.get_model().objects
+
+
+class CreatureIndex(indexes.SearchIndex, indexes.Indexable):
+  # Declare which model attributes are included in indexing/searching
+  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
+  name = indexes.CharField(model_attr='name')
+  description = indexes.CharField(model_attr='description')
+
+  def get_model(self):
+      return Creature
 
   def index_queryset(self, using=None):
       """
