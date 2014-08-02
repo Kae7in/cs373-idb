6fb6d5bb5a45f89dd097e14592cb76caf775514d
Kaelin <kae7in@gmail.com>
2014-08-02 16:06:04 -0500

    fixed errors with potions

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -55,9 +55,10 @@ class LocationIndex(indexes.SearchIndex, indexes.Indexable):
 class PotionIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
-  title = indexes.CharField(model_attr='title')
+  name = indexes.CharField(model_attr='title')
   effects = indexes.CharField(model_attr='effects')
   recipe = indexes.CharField(model_attr='recipe')
+  description = indexes.CharField(model_attr='effects')
 
   def get_model(self):
       return Potion
@@ -68,27 +69,6 @@ class PotionIndex(indexes.SearchIndex, indexes.Indexable):
       that don't need refiltering?
       """
       return self.get_model().objects
-
-
-class IngredientIndex(indexes.SearchIndex, indexes.Indexable):
-  # Declare which model attributes are included in indexing/searching
-  text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
-  name = indexes.CharField(model_attr='name')
-  creature = indexes.CharField(model_attr='creature')
-
-  def get_model(self):
-      return Ingredient
-
-  def prepare_author(self, obj):
-    return obj.creature.name
-
-  def index_queryset(self, using=None):
-      """
-      Used when the entire index for model is updated. You can filter out things
-      that don't need refiltering?
-      """
-      return self.get_model().objects
-
 
 class BookIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
@@ -101,14 +81,14 @@ class BookIndex(indexes.SearchIndex, indexes.Indexable):
       return Book
 
   def prepare_subjects(self, obj):
-    return [c.name for c in obj.subjects.all()]
+      return [c.name for c in obj.subjects.all()]
 
   def index_queryset(self, using=None):
       """
       Used when the entire index for model is updated. You can filter out things
       that don't need refiltering?
       """
-    return self.get_model().objects
+      return self.get_model().objects
 
 class ArtifactIndex(indexes.SearchIndex, indexes.Indexable):
    text = indexes.CharField(document=True, use_template=True)
@@ -120,4 +100,4 @@ class ArtifactIndex(indexes.SearchIndex, indexes.Indexable):
        return Artifact
 
    def index_queryset(self, using=None):
-      return self.get_model().objects
+       return self.get_model().objects
