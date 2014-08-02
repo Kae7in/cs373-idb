9587cc428afdcdcfac9c5754bc8355a487d5704f
Angela Hsu <ahsu@cs.utexas.edu>
2014-08-02 16:38:09 -0500

    cleaned up some search models

--- hogwarts/marauder/search_indexes.py
+++ hogwarts/marauder/search_indexes.py
@@ -5,7 +5,6 @@ class CharacterIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
   name = indexes.CharField(model_attr='name')
-  wand = indexes.CharField(model_attr='wand')
   description = indexes.CharField(model_attr='description')
 
   def get_model(self):
@@ -39,7 +38,6 @@ class LocationIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
   name = indexes.CharField(model_attr='name')
-  kind = indexes.CharField(model_attr='kind')
   description = indexes.CharField(model_attr='description')
 
   def get_model(self):
@@ -56,8 +54,6 @@ class PotionIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
   name = indexes.CharField(model_attr='title')
-  effects = indexes.CharField(model_attr='effects')
-  recipe = indexes.CharField(model_attr='recipe')
   description = indexes.CharField(model_attr='effects')
 
   def get_model(self):
@@ -74,6 +70,7 @@ class BookIndex(indexes.SearchIndex, indexes.Indexable):
   # Declare which model attributes are included in indexing/searching
   text = indexes.CharField(document=True, use_template=True) # This is the index document Haystack will build for searching
   name = indexes.CharField(model_attr='name')
+  description = indexes.CharField(model_attr='description')
 
   def get_model(self):
       return Book
@@ -89,7 +86,6 @@ class ArtifactIndex(indexes.SearchIndex, indexes.Indexable):
    text = indexes.CharField(document=True, use_template=True)
    name = indexes.CharField(model_attr='name')
    description = indexes.CharField(model_attr='description')
-   kind = indexes.CharField(model_attr='kind')
 
    def get_model(self):
        return Artifact
